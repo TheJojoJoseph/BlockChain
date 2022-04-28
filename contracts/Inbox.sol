a0c7cfa0adec0dd7fc2969bc2d0e557a9e01e7b2
jojojoseph <jojo.joseph@factweavers>
2022-04-28 12:47:09 +0530

    compile code added

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,4 +1,21 @@
 pragma solidity ^0.4.17;
-// linter warnings (red underline) about pragma version can igonored!
 
-// contract code will go here
+contract Inbox{
+    string public message;
+
+    function Inbox(string initialMessage)public {
+        message = initialMessage;
+    }
+    function setMessage(string initialMessage)public {
+        message = initialMessage;
+    }   
+    function getMessage() public view returns (string) {
+        return message;
+    } 
+
+    function doMath(int a, int b) {
+        a+b;
+        a-b;
+        a == 0; 
+    }
+}
