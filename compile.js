1759c49bf7a46ed62fc0d3c701094d3f5a2c9dd0
jojojoseph <jojo.joseph@factweavers>
2022-04-29 16:16:26 +0530

    test functions messages and setmessages are tested using mocha wirh ganache-cli running

--- compile.js
+++ compile.js
@@ -7,6 +7,6 @@ const inboxPath = path.resolve(__dirname,'contracts','Inbox.sol');
 const source = fs.readFileSync(inboxPath,'utf8');
 
 
-console.log(solc.compile(source,1));
+//console.log(solc.compile(source,1));
 
 module.exports = solc.compile(source,1).contracts[':Inbox'];
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,35 @@
-// contract test code will go here
+const assert = require("assert");  //comparison 
+const ganache = require("ganache-cli");  //
+const { interfaces } = require("mocha");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+const { interface , bytecode } = require('../compile') 
+
+
+let accounts;
+
+beforeEach( async () => {   
+    //Get a list of all accounts
+    accounts = await web3.eth.getAccounts()
+     
+    //We will use pne of those accounts to deploy contract 
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({ data : bytecode ,arguments : ['Hi there']})
+      .send({ from :accounts[0] , gas : '1000000'})  
+});
+
+describe("Inbox", () => {
+  it("deploy contract", () => {
+   // console.log (inbox);
+    assert.ok(inbox.options.address);
+  });
+  it ("has  a default message ", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there');
+  });
+  it("set message", async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0]} )
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
+  })
+});
